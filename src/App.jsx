04c3af457e540cf63a5237cf72afb513a3890288
zoniha <awaawapurin@gmail.com>
2022-04-10 03:37:16 +0900

    ✨ feat: Display user card

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
 import styled from 'styled-components';
 import { SearchInput } from './components/molecules/SearchInput';
+import { UserCard } from './components/organisms/user/UseCard';
+
+export const App = () => {
+  return (
+    <StLayout>
+      <h1>Hi!</h1>
+      <SearchInput />
+      <UserCard />
+    </StLayout>
+  );
+};
 
 const StLayout = styled.div`
   display: flex;
@@ -9,14 +20,3 @@ const StLayout = styled.div`
   align-items: center;
   margin-top: 100px;
 `;
-
-const App = () => {
-  return (
-    <StLayout>
-      <h1>Hi!</h1>
-      <SearchInput />
-    </StLayout>
-  );
-};
-
-export default App;
