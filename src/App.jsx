c520be361d19719154beb136e244ba3ad12e9c00
zoniha <awaawapurin@gmail.com>
2022-04-09 22:45:21 +0900

    ☪️ refactor: Update

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,10 @@ const Layout = styled.div`
   margin-top: 100px;
 `;
 
-const App = () => {
+export const App = () => {
   return (
     <Layout>
       <h1>Hi!</h1>
     </Layout>
   );
 };
-
-export default App;
