609a54f9b2a5e2f8feacd26accdeebe93d8d6ab1
zoniha <awaawapurin@gmail.com>
2022-04-10 03:37:00 +0900

    🎉 init: Initial commit

---
+++ src/components/organisms/user/UseCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img alt="Profile" />
+      <p>NAME</p>
+      <dl>
+        <dt>MAIL</dt>
+        <dd>xxx@xxx.com</dd>
+        <dt>TEL</dt>
+        <dd>xx-xxx-xxxx</dd>
+        <dt>JOB</dt>
+        <dd>Fisher</dd>
+        <dt>WEB</dt>
+        <dd>xxx.com</dd>
+      </dl>
+    </div>
+  );
+};
